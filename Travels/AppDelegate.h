56cd860995f1ee93efd02110a546a818ad748f66
stonedong <yishuiliunian@gmail.com>
2014-09-23 14:23:44 +0800

    Initial Commit

---
+++ Travels/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Travels
+//
+//  Created by stonedong on 14-9-23.
+//  Copyright (c) 2014年 stonedong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TravelsTests/TravelsTests.m
@@ -0,0 +1,40 @@
+//
+//  TravelsTests.m
+//  TravelsTests
+//
+//  Created by stonedong on 14-9-23.
+//  Copyright (c) 2014年 stonedong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface TravelsTests : XCTestCase
+
+@end
+
+@implementation TravelsTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
